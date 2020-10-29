e5eedb2c50b4b05cd5ef48caac58fecde56ae122
2019stu08 <2019stu08@compB06.nagoya-vti.ac.jp>
2020-10-29 15:09:48 +0900

    commit3

--- newfile.php
+++ newfile.php
@@ -19,14 +19,14 @@ form dl dd{
    </style>
  </head>
  <body>
-   <h1>イケメン2</h1>
+   <h1>てらたく</h1>
    <form  action="login.php" method="post">
    <dl>
      <dt>ユーザー名</dt>
      <dd><input type="text" name="id" size="50"></dd>
      <dt>パスワード</dt>
      <dd><input type="password" name="password" size="51"></dd><br>
-     <button type="submit">いそっち2</button>
+     <button type="submit">てらたく</button>
    </dl>
    </form>
  </body>
